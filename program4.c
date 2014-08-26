dd77fd04f0ea0e5942ec007d899206a2d51736be
sengolmary <sengol0409@gmail.com>
2014-08-26 17:18:23 +0530

    c3

---
+++ program4.c
@@ -0,0 +1,14 @@
+/* chapter 3- program 4 *
+* sengolmary august 25 2014 */
+
+#include<stdio.h>
+
+int main(void)
+{
+	int value1, value2, sum;
+	value1 = 50;
+	value2 = 25;
+	sum = value1 + value2;
+	printf ("the sum is \n", value1, value2, sum);
+	return 0;
+}
